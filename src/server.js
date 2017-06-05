69a113a605f92e9c5b23a1e93bbd5bb42f8b1bf5
Yashika Garg <yashikagarag13@gmail.com>
2017-06-05 13:19:41 +0530

    FB Bot - Sample send text message

--- src/server.js
+++ src/server.js
@@ -8,16 +8,42 @@ const app = express();
 app.use(bodyParser.text({type: 'application/json'}));
 
 app.post('/webhook/', (req, res) => {
+    console.log('hook request');
     try {
-        console.log('req', req);
-        console.log('res', res);
-        return res.status(200).json({
-            status: "ok"
+        var speech = 'empty speech';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action) {
+                    speech += 'action: ' + requestBody.result.action;
+                }
+            }
+        }
+
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
         });
     } catch (err) {
+        console.error("Can't process request", err);
+
         return res.status(400).json({
-            status: "error",
-            error: err
+            status: {
+                code: 400,
+                errorType: err.message
+            }
         });
     }
 
@@ -26,3 +52,6 @@ app.post('/webhook/', (req, res) => {
 app.listen(REST_PORT, () => {
     console.log('Rest service ready on port ' + REST_PORT);
 });
+
+// curl 'https://api.api.ai/api/query?v=20150910&query=Hi&lang=en&sessionId=5adc0dba-bcb2-4889-a93e-2c92eaaad3dc&
+// timezone=2017-06-05T11:13:13+0530' -H 'Authorization:Bearer 052b4926d06f40d78a0742e3d3d44d05'

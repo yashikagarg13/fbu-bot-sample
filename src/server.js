e84db0741893ca90a9735875981a5285ef1c124a
Yashika Garg <yashikagarag13@gmail.com>
2017-06-07 13:08:14 +0530

    Webhook - Card Message (Trial 14)

--- src/server.js
+++ src/server.js
@@ -78,7 +78,7 @@ app.post('/webhook/', (req, res) => {
             source: 'apiai-webhook-sample',
             data: {
               facebook: {
-                message
+                message: "Hello World!!"
               }
             },
         });

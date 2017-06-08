88eda2b8e2a187043dc92e22dc0fd1b66b5fcb61
Yashika Garg <yashikagarag13@gmail.com>
2017-06-08 12:27:29 +0530

    Webhook - using apiai

--- src/helpers/processMessage.js
+++ src/helpers/processMessage.js
@@ -4,7 +4,7 @@ const CAT_IMAGE_URL = "https://botcube.co/public/blog/apiai-tutorial-bot/hosico_
 const apiAiClient = require('apiai')(APIAI_ACCESS_TOKEN);
 const request = require('request');
 
-const sendTextMessage = (senderId, text) => {
+const sendCardMessage = (senderId, text) => {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: FB_PAGE_ACCESS_TOKEN },
@@ -15,10 +15,11 @@ const sendTextMessage = (senderId, text) => {
         attachment: {
           type: "template",
           payload: {
-            template_type: "list",
+            template_type: "generic",
             elements: [{
               title: "Test Job",
               subtitle: "Test job subtitle",
+              image_url: CAT_IMAGE_URL,
               buttons: [{
                 title: "Apply",
                 url: "http://google.com"
@@ -35,7 +36,7 @@ module.exports = (event) => {
     const senderId = event.sender.id;
     const message = event.message.text;
 
-    sendTextMessage(senderId);
+    sendCardMessage(senderId);
 
     /*const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'botcube_co'});
 

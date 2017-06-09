ddf67ba23dd571f0bcc528483b9951af2e70ddcc
Yashika Garg <yashikagarag13@gmail.com>
2017-06-09 11:35:36 +0530

    Webhook - call PTF api

--- src/helpers/processMessage.js
+++ src/helpers/processMessage.js
@@ -8,7 +8,7 @@ const rp = require('request-promise-native');
 const sendCardMessage = (senderId, category) => {
   console.log('sendCardMessage', "http://localhost:5000/api/v1/jobs/?per_page=3&page=0&filter=category_id==45");
   return rp({
-      uri: "https://qa.powertofly.com/api/v1/jobs/?per_page=3&page=0&filter=category_id==45&fields=id,title,header_image_name",
+      uri: "http://127.0.0.1:5000/api/v1/jobs/?per_page=3&page=0&filter=category_id==45&fields=id,title,header_image_name",
       json: true,
       method: "GET",
       // headers

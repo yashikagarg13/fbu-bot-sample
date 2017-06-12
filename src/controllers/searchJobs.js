e5b401fd82b659cf594ef8f1854b589034da3d37
Yashika Garg <yashikagarag13@gmail.com>
2017-06-12 14:19:07 +0530

    Webhook - call fbu api

--- src/controllers/searchJobs.js
+++ src/controllers/searchJobs.js
@@ -3,55 +3,57 @@ const {APIAI_ACCESS_TOKEN, FB_PAGE_ACCESS_TOKEN} = require("../config");
 const rp = require('request-promise-native');
 
 module.exports = (req, res) => {
-  const result = res.result.fulfillment.speech;
-  const category = res.result.parameters.category;
+  const action = req.body.result.action;
+  const category = req.body.result.parameters["category"];
 
-  return rp({
-    uri: "https://powertofly.com/api/v1/categories/",
-    json: true,
-    method: "GET",
-    headers: {
-      "Authorization": "Bearer 10rJS0M6ZHJ9vCPlRVWYHAdlioDfSC"
-    },
-  }).then(function (response) {
-    let categoryId = response.data.filter(c => c.title === category)[0].id;
-    if (categoryId) {
-      return rp({
-        uri: "https://powertofly.com/api/v1/jobs/?status=Active&per_page=3&page=0&filter=category_id==" +
-          categoryId + "&fields=id,title,header_image_name",
-        json: true,
-        method: "GET",
-        headers: {
-          "Authorization": "Bearer 10rJS0M6ZHJ9vCPlRVWYHAdlioDfSC"
-        },
-      });
-    } else {
-      return res.json({
-        speech: "No jobs found under " + category + " category",
-        displayText: "No jobs found under " + category + " category",
-        source: 'search-job'
-      });
-    }
-  }).then(function (response) {
-    let jobs = response.data;
-    let total = response.meta.total;
+  if (action == "search-job" && category) {
+    return rp({
+      uri: "https://powertofly.com/api/v1/categories/",
+      json: true,
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer 10rJS0M6ZHJ9vCPlRVWYHAdlioDfSC"
+      },
+    }).then(function (response) {
+      let categoryId = response.data.filter(c => c.title === category)[0].id;
+      if (categoryId) {
+        return rp({
+          uri: "https://powertofly.com/api/v1/jobs/?status=Active&per_page=3&page=0&filter=category_id==" +
+            categoryId + "&fields=id,title,header_image_name",
+          json: true,
+          method: "GET",
+          headers: {
+            "Authorization": "Bearer 10rJS0M6ZHJ9vCPlRVWYHAdlioDfSC"
+          },
+        });
+      } else {
+        return res.json({
+          speech: "No jobs found under " + category + " category",
+          displayText: "No jobs found under " + category + " category",
+          source: 'search-job'
+        });
+      }
+    }).then(function (response) {
+      let jobs = response.data;
+      let total = response.meta.total;
 
-    if (total == 0) {
-      return res.json({
-        speech: "No jobs found under " + category + " category",
-        displayText: "No jobs found under " + category + " category",
-        source: 'search-job'
-      });
-    } else {
-      return res.json({
-        speech: "Here are top 3 jobs under " + category + " category",
-        displayText: "Here are top 3 jobs under " + category + " category",
-        source: 'search-job'
-      });
-    }
-  }).then(function (response) {
-    console.log('response', response);
-  }).catch(function (error) {
-    console.log('error', error);
-  });
+      if (total == 0) {
+        return res.json({
+          speech: "No jobs found under " + category + " category",
+          displayText: "No jobs found under " + category + " category",
+          source: 'search-job'
+        });
+      } else {
+        return res.json({
+          speech: "Here are top 3 jobs under " + category + " category",
+          displayText: "Here are top 3 jobs under " + category + " category",
+          source: 'search-job'
+        });
+      }
+    }).then(function (response) {
+      console.log('response', response);
+    }).catch(function (error) {
+      console.log('error', error);
+    });
+  }
 };
